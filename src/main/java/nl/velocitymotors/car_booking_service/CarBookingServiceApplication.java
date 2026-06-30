30b3c86cfb973b4f25a69a978790806db7f1acb8
Marco <marco@laptop.local>
2026-06-30 14:08:16 +0200

    feat: first commit, repo created with spring initializr

---
+++ src/main/java/nl/velocitymotors/car_booking_service/CarBookingServiceApplication.java
@@ -0,0 +1,13 @@
+package nl.velocitymotors.car_booking_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarBookingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarBookingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/velocitymotors/car_booking_service/CarBookingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.velocitymotors.car_booking_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarBookingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
